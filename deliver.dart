ccdaf8f3f82ce427c547681d9bf116ef5984f8c1
smit.patel1305 <142987614+Smit1025@users.noreply.github.com>
2024-04-26 22:26:08 +0530

    Add files via upload

---
+++ deliver.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class welcomescreen extends StatelessWidget {
+  const welcomescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Image.asset("assets/images/boy-removebg-preview.png"),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "Order You Food Now!",
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.italic,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              "Order food and get delivery withing a few minuts to your door",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black45,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 80),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ));
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+              padding: EdgeInsets.symmetric(vertical: 20),
+              decoration: BoxDecoration(
+                color: Color(0xFFFF2F08),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Get Strated",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                    size: 20,
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  HomeScreen() {}
+}
